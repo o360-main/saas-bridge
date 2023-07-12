cce888de9f86d444e12cdb05ed71021e622fe985
vimal-360 <vimal@o360integrator.com>
2023-07-12 07:55:57 +0530

    wip

--- src/Contracts/ControllerInterface.php
+++ src/Contracts/ControllerInterface.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 interface ControllerInterface
 {
+    //Routes : /api/{module}/config
     public function config(Request $request = null);
 
+    //Routes : /api/{module}/data
+    public function data(Request $request = null);
+
+    //Routes : /api/{module}/import
     public function import(Request $request = null);
 
+    //Routes : /api/{module}/export
     public function export(Request $request = null);
 
     public function trigger(Request $request = null);
--- src/Facades/SaasBridge.php
+++ src/Facades/SaasBridge.php
@@ -7,10 +7,10 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static \Illuminate\Http\Client\PendingRequest api($version = null)
  * @method static array credentials()
- * @method static \O360Main\SaasBridge\SaasBridgeService getInstance()
  * @method static array config()
  * @method static array configurations()
  * @method static array moduleConfig()
+ * @method static \O360Main\SaasBridge\SaasBridgeService getInstance()
  * @see \O360Main\SaasBridge\SaasBridgeService
  */
 class SaasBridge extends Facade
