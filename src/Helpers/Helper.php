18d367eedeffbf0e7aeae80828334bf3ec223cce
vimal-360 <vimal@o360integrator.com>
2023-10-06 12:25:09 +0530

    wip

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -14,4 +14,19 @@ class Helper
         return implode('/', array_map(fn($arg) => trim($arg, '/'), $path));
     }
 
+
+    public static function deepArrDefault(array $arr, array $defaultArr = []): array
+    {
+        foreach ($defaultArr as $key => $value) {
+            if (!isset($arr[$key])) {
+                $arr[$key] = $value;
+            } else {
+                if (is_array($value)) {
+                    $arr[$key] = self::deepArrDefault($arr[$key], $value);
+                }
+            }
+        }
+
+        return $arr;
+    }
 }
---
+++ src/Responses/ImportResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace O360Main\SaasBridge\Responses;
+
+
+use Illuminate\Contracts\Support\Responsable;
+
+class ImportResponse implements Responsable
+{
+
+    public function __construct(
+        public bool    $status = false,
+        public ?string $message = null,
+        public array   $data = [],
+    )
+    {
+    }
+
+    public function toResponse($request): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'data' => $this->data,
+            'status' => $this->status,
+            'message' => $this->message,
+        ]);
+    }
+}
