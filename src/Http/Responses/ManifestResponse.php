c59dfc2fe372f76a437ec1365d20deabd3fc24df
vimal-360 <vimal@o360integrator.com>
2023-10-18 02:17:05 +0530

    wip

--- src/Http/Responses/ManifestResponse.php
+++ src/Http/Responses/ManifestResponse.php
@@ -2,12 +2,125 @@
 
 namespace O360Main\SaasBridge\Http\Responses;
 
+use Exception;
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Http\JsonResponse;
+use O360Main\SaasBridge\Http\Responses\Manifest\ManifestConfig;
+use O360Main\SaasBridge\Http\Responses\Manifest\ManifestCredentialForm;
+use O360Main\SaasBridge\Http\Responses\Manifest\ManifestDeveloper;
+use O360Main\SaasBridge\Http\Responses\Manifest\ManifestLogo;
+use O360Main\SaasBridge\Http\Responses\Manifest\PluginType;
 
-class ManifestResponse implements Responsable
+class ManifestResponse implements Responsable, Arrayable
 {
 
+    //for new format -> default v1
+    public string $manifest_version = "v2";
 
+    /**
+     * @throws \Exception
+     */
+    public function __construct(
+        public readonly string             $name,
+        public readonly string             $display_name,
+        public readonly string             $base_url,
+        public readonly string             $manifest_url,
+        public readonly ManifestLogo       $logo,
+        public readonly PluginType         $type,
+        public readonly string             $description,
+        public readonly string             $version,
+        public readonly array              $tags,
+        public readonly ?ManifestDeveloper $developer,
+        public readonly ?ManifestConfig    $config,
+        public readonly array              $options,
+    )
+    {
+
+        //check $this->version must follow samvar -> https://semver.org/
+
+        if (!preg_match('/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$/', $this->version)) {
+            throw new Exception('version -> Invalid version, version must follow semvar');
+        }
+
+
+        if (!filter_var($this->base_url, FILTER_VALIDATE_URL)) {
+            throw new Exception('base_url -> Invalid BaseUrl URL');
+        }
+
+        if (!filter_var($this->manifest_url, FILTER_VALIDATE_URL)) {
+            throw new Exception('manifest_url -> Invalid Manifest URL');
+        }
+
+        //check tags contain only string
+        foreach ($this->tags as $tag) {
+            if (!is_string($tag)) {
+                throw new Exception('tags -> Invalid tags, tags must be array of strings only');
+            }
+        }
+
+
+        foreach ($this->options as $option) {
+
+            if (!is_a($option, ManifestCredentialForm::class)) {
+                throw new Exception('options -> Invalid ManifestCredential -> ' . get_class($option) . ' is not a ManifestCredential');
+            }
+        }
+
+    }
+
+    public function toArray(): array
+    {
+
+        $version = config('saas-bridge.main_version');
+
+
+        if ($version == "v1") {
+            //old
+
+            return [
+                'name' => $this->name,
+                'display_name' => $this->display_name,
+                'base_url' => $this->base_url,
+                'manifest_url' => $this->manifest_url,
+                'logo' => $this->logo->toArray(),
+                'type' => $this->type->value,
+                'description' => $this->description,
+                'version' => $this->version,
+                'tags' => $this->tags,
+                'developer' => $this->developer?->toArray(),
+                'config' => $this->config?->toArray(),
+                'options' => [
+                    'add' => collect($this->options)->mapWithKeys(fn($i) => [$i->key => $i->toArray()])->toArray(),
+                    'remove' => [],
+                ],
+            ];
+        }
+
+
+        return [
+            'manifest_version' => $this->manifest_version,
+            'name' => $this->name,
+            'display_name' => $this->display_name,
+            'base_url' => $this->base_url,
+            'manifest_url' => $this->manifest_url,
+            'logo' => $this->logo->toArray(),
+            'type' => $this->type->value,
+            'description' => $this->description,
+            'version' => $this->version,
+            'tags' => $this->tags,
+            'developer' => $this->developer?->toArray(),
+            'config' => $this->config?->toArray(),
+            'options' => collect($this->options)->map(fn($item) => $item->toArray())->toArray(),
+        ];
+    }
+
+
+    /**
+     * @param $filePath
+     * @return \Illuminate\Http\JsonResponse|void
+     * @deprecated Use ManifestResponse as a replacement
+     */
     public static function json($filePath)
     {
         if (!file_exists($filePath)) {
@@ -18,14 +131,14 @@ class ManifestResponse implements Responsable
 
             return response()->json($json);
 
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             abort(500, 'Invalid JSON');
         }
     }
 
 
-    public function toResponse($request): array
+    public function toResponse($request): JsonResponse
     {
-        return [];
+        return response()->json($this->toArray());
     }
 }
--- src/Http/Responses/TriggerResponse.php
+++ src/Http/Responses/TriggerResponse.php
@@ -11,12 +11,13 @@ class TriggerResponse implements Responsable
     public function __construct(
         protected readonly bool        $completed,
         protected readonly int|null    $progress_in_percentage = null,
-        protected readonly int         $interval_in_seconds = 300,
+        protected readonly int|null    $interval_in_seconds = null,
         protected readonly bool        $isError = false,
         protected readonly string|null $errorMsg = null,
         protected readonly array       $data = []
     )
     {
+        $interval_in_seconds ??= 300;
     }
 
     public function toResponse($request): \Illuminate\Http\JsonResponse
