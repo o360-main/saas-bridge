b9f1e3951f1c749496acf327e98faad156016a30
vimal-360 <vimal@o360integrator.com>
2023-10-25 10:03:57 +0530

    wip

--- config/saas-bridge.php
+++ config/saas-bridge.php
@@ -5,7 +5,8 @@
  */
 return [
     'strict_mode' => env('SAAS_BRIDGE_STRICT_MODE', false),
-    'main_version' => env('SAAS_BRIDGE_MAIN_VERSION', 'v1'),
+//    'main_version' => env('SAAS_BRIDGE_MAIN_VERSION', 'v1'),
+    'main_version' => 'v1',
     'saas_api_url' => env('SAAS_API_URL', 'http://core.o360-core.test'),
     'plugin_secret' => env('PLUGIN_SECRET', 'secret'),
     'token_validate_endpoint' => "/connection/validate",
--- src/Http/Responses/ManifestResponse.php
+++ src/Http/Responses/ManifestResponse.php
@@ -116,6 +116,7 @@ class ManifestResponse implements Responsable, Arrayable
 
     /**
      * @param $filePath
+     * @return \O360Main\SaasBridge\Http\Responses\ManifestResponse|void
      * @deprecated Use ManifestResponse as a replacement
      */
     public static function fromJsonFile($filePath)
