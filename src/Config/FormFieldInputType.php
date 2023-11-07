b28b7ef4bdf41698fea57c407f0e67432ed6c957
vimal-360 <vimal@o360integrator.com>
2023-11-07 15:18:21 +0530

    wip

--- src/Config/FormFieldInputType.php
+++ src/Config/FormFieldInputType.php
@@ -6,10 +6,18 @@ enum FormFieldInputType: string
 {
     case text = 'text';
     case number = 'number';
+    case email = 'email';
+    case password = 'password';
     case select = 'select';
-    case radio = 'radio';
     case checkbox = 'checkbox';
+    case radio = 'radio';
     case textarea = 'textarea';
+    case file = 'file';
+    case image = 'image';
+    case color = 'color';
     case date = 'date';
+    case time = 'time';
+    case datetime = 'datetime';
+    case datetime_local = 'datetime_local';
 
 }
