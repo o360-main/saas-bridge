202dee576bbb55769b39ccafb381c087cef3e1d9
vimal-360 <vimal@o360integrator.com>
2023-11-21 15:55:22 +0530

    fix

--- src/Http/Responses/ExportResponse.php
+++ src/Http/Responses/ExportResponse.php
@@ -7,17 +7,18 @@ use Illuminate\Contracts\Support\Responsable;
 class ExportResponse implements Responsable
 {
     public function __construct(
-        public bool    $status = false,
+        public bool    $success = false,
         public ?string $message = null,
         public array   $data = [],
-    ) {
+    )
+    {
     }
 
     public function toResponse($request): \Illuminate\Http\JsonResponse
     {
         return response()->json([
             'data' => $this->data,
-            'status' => $this->status,
+            'success' => $this->success,
             'message' => $this->message,
         ]);
     }
