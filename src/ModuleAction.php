e1862b67ef418915480628bcfbc30f8f73fa5c4b
vimal-360 <vimal@o360integrator.com>
2024-04-11 08:56:45 +0530

    feat: import actions

--- src/ModuleAction.php
+++ src/ModuleAction.php
@@ -5,5 +5,6 @@ namespace O360Main\SaasBridge;
 enum ModuleAction: string
 {
     case import_all = 'import_all';
-
+    case import_only = 'import_only';
+    case import_by_date = 'import_by_date';
 }
