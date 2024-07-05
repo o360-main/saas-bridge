1ac2ba2d86aa9125eff4e7fc42b57c04952a7089
vimal-360 <vimal@o360integrator.com>
2024-07-05 14:07:15 +0530

    wip

--- src/Services/SaasCredentialsBoot.php
+++ src/Services/SaasCredentialsBoot.php
@@ -149,9 +149,7 @@ class SaasCredentialsBoot
 //        "2.0.0"
         $headers['X-Main-Version'] = $this->environment['version'] ?? "1.0.0";
 
-        if ($this->environment['version'] != "1.0.0") {
-            $headers['X-Plugin-Id'] = $pluginId;
-        }
+        $headers['X-Plugin-Id'] = $pluginId;
 
         //Set headers
         $headers = [
