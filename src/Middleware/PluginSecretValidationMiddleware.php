32ae413fca1ce1d3866fb20b71f3096a225c128d
Vimal Mistry <vimal@saasintegrator.com>
2024-08-29 01:58:21 +0530

    wip

--- src/Middleware/PluginSecretValidationMiddleware.php
+++ src/Middleware/PluginSecretValidationMiddleware.php
@@ -22,7 +22,7 @@ class PluginSecretValidationMiddleware
         abort_if(
             empty($token),
             401,
-            'Plugin secret not set [0]'
+            'Plugin secret not set [0xx]'
         );
 
         SaasCredentialsBoot::setEnvironment($request);
