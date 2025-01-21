b49835430e386a23982ced34907d0330e47c7aa5
Vimal Mistry <vimal@saasintegrator.com>
2025-01-21 07:42:15 +0530

    wip

--- src/Http/Responses/TestCredentialsResponse.php
+++ src/Http/Responses/TestCredentialsResponse.php
@@ -7,19 +7,17 @@ use Illuminate\Contracts\Support\Responsable;
 class TestCredentialsResponse implements Responsable
 {
     public function __construct(
-        protected bool   $success,
+        protected bool $success,
         protected string $message,
-        protected array  $data = []
-    ) {
-    }
-
+        protected array $data = []
+    ) {}
 
     public function toResponse($request): array
     {
         return [
             'success' => $this->success,
             'message' => $this->message,
-            'data' => $this->data
+            'data' => $this->data,
         ];
     }
 }
