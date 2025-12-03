e07399e15d8b164d78f2ae782031873e55910a60
vimal-360 <95009456+vimal-360@users.noreply.github.com>
2025-12-03 15:10:27 +0530

    wip

--- src/Services/SaasCredentialsBootV1.php
+++ src/Services/SaasCredentialsBootV1.php
@@ -151,6 +151,6 @@ class SaasCredentialsBootV1
         $this->saasAgent->setDataConfig($data['data_config'] ?? []);
         $this->saasAgent->setSource($data['source'] ?? $data['source_of_truth'] ?? $data['main_modules'] ?? []);
         $this->saasAgent->setEnabled($data['enabled_modules'] ?? []);
-        $this->saasAgent->setMetaDataConfig($data['meta_data_config'] ?? []);
+        $this->saasAgent->setMetaDataConfig($data['metadata_config'] ?? $data['meta_data_config'] ?? []);
     }
 }
